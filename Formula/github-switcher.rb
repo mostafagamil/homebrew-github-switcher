15e542f1cb1ad74acf039fd7bffd37e9174cc12c
Mostafa Gamil <mostafa_gamil@yahoo.com>
2025-09-06 17:27:40 +0300

    fix: simplify Homebrew formula to auto-resolve dependencies
    - Remove manual resource definitions that were causing 404 errors
    - Let Homebrew's virtualenv_install_with_resources handle all dependencies automatically
    - This approach is more reliable and maintainable

--- Formula/github-switcher.rb
+++ Formula/github-switcher.rb
@@ -9,66 +9,6 @@ class GithubSwitcher < Formula
 
   depends_on "python@3.11"
 
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/source/c/click/click-8.1.7.tar.gz"
-    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
-  end
-
-  resource "cryptography" do
-    url "https://files.pythonhosted.org/packages/source/c/cryptography/cryptography-42.0.5.tar.gz"
-    sha256 "6fe07eec95dfd477eb9530aef5bead34fec819b3aaf6c5bd6d20565da607bfe1"
-  end
-
-  resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/source/m/markdown-it-py/markdown_it_py-3.0.0.tar.gz"
-    sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
-  end
-
-  resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/source/m/mdurl/mdurl-0.1.2.tar.gz"
-    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
-  end
-
-  resource "paramiko" do
-    url "https://files.pythonhosted.org/packages/source/p/paramiko/paramiko-3.4.0.tar.gz"
-    sha256 "aac08f26a31dc4dffd92821527d1682d99d52f9ef6851968114a8728f3c274d3"
-  end
-
-  resource "pycparser" do
-    url "https://files.pythonhosted.org/packages/source/p/pycparser/pycparser-2.21.tar.gz"
-    sha256 "e644fdec12f7872f86c58ff790da456218b10f863970249516d60a5eaca77206"
-  end
-
-  resource "pygments" do
-    url "https://files.pythonhosted.org/packages/source/p/pygments/pygments-2.17.2.tar.gz"
-    sha256 "da46cec9fd2de5be3a8a784f434e4c4ab670b4ff54d605c4c2717e9d49c4c367"
-  end
-
-  resource "pyperclip" do
-    url "https://files.pythonhosted.org/packages/source/p/pyperclip/pyperclip-1.8.2.tar.gz"
-    sha256 "105254a8b04934f0bc84e9c24eb360a591aaf6535c9def5f29d92af107a9bf57"
-  end
-
-  resource "rich" do
-    url "https://files.pythonhosted.org/packages/source/r/rich/rich-13.7.1.tar.gz"
-    sha256 "9be308cb1fe2f1f57d67ce99e95af38a1e2bc6b56c9917b87e2c4c7c58fd6d8"
-  end
-
-  resource "shellingham" do
-    url "https://files.pythonhosted.org/packages/source/s/shellingham/shellingham-1.5.4.tar.gz"
-    sha256 "8dbca0739d487e5bd35ab3ca4b36e11c4078f3a234bfce294b0a0291363404de"
-  end
-
-  resource "toml" do
-    url "https://files.pythonhosted.org/packages/source/t/toml/toml-0.10.2.tar.gz"
-    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
-  end
-
-  resource "typer" do
-    url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.12.3.tar.gz"
-    sha256 "49e73131481d804288ef62598d97a1ceef3058905aa536a1134f90891ba35482"
-  end
-
   def install
     virtualenv_install_with_resources
   end
